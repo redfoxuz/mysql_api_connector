bfea8ea5fa4933bb65e17cbc22398764935c432f
redfoxuz <166968150+redfoxuz@users.noreply.github.com>
2026-03-11 17:48:59 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,130 @@
+#butun sonlar
+from sqlalchemy.orm.sync import populate
+
+# a = 5
+# b = 10
+# c = -3
+#
+# print(a)
+# print(b)
+# print(c)
+#
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+#FLOAT kasrli son
+# pi = 3.14
+# price = 10.5
+# temp = - 2.7
+# print(type(pi))
+# print(type(price))
+# print(type(temp))
+#
+# # Matnlar str
+#
+# name = "Ali"
+# city = 'Tashkent'
+# text = 'Men pythonist man'
+# print(type(name))
+# print(type(city))
+# print(type(text))
+
+#Boolen True yoki False
+
+# a = True
+# b = False
+# print(type(a))
+# print(type(b))
+
+#
+# s = '10'
+# # print(type(s))
+# n = int(s)
+# print(type(n))
+
+# n = 5
+# print(type(n))
+
+# f = str(n)
+# print(type(f))
+
+
+# print(bool(1))
+# print(bool(0))
+
+# print(bool("Salom"))
+# print(bool(""))
+
+# population = 1_000_000
+# print(population)
+# print(type(population))
+
+
+# n = 1.5e6
+# b = int(n)
+# print(n)
+# print(type(b))
+
+# complex son
+# z = 3 + 5j
+# # print(type(z))
+# print(z)
+
+
+# x = None
+# print(type(x))
+
+# print(10 / 2)
+# print(type(10 / 2))
+
+# print(12 // 3)
+# print(type(10 // 3))
+
+# x = 10.12
+#
+# if isinstance(x, int):
+#     print("bu butun son ")
+# if isinstance(x, float):
+#     print("bu kasr son ")
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+#
+# if isinstance(name, str):
+#     print("Ism to'gri")
+# if isinstance(age, int):
+#     print("Yosh to'gri")
+#
+
+# person  = '123'
+#
+# if isinstance(person, str):
+#     print("Salom John !")
+# elif isinstance(person, int):
+#     print("Siz kimsiz ?")
+# else:
+#     print("Hato 404")
+#
+
+# score = 150
+# if isinstance(score, int):
+#     print("Sizning balingiz: ", score)
+#     if score > 100:
+#         print("^^^^^ Level up ^^^^^^", score)
+
+
+# data = [10, 'python', 3.5, True]
+#
+# for item in data:
+#     if isinstance(item, str):
+#         print(item,"--> Bu matn !")
+#     elif isinstance(item, int):
+#         print(item,"--> Bu butun son !")
+#     elif isinstance(item, float):
+#         print(item,"--> Bu kasr son !")
+#     elif isinstance(item, bool):
+#         print(item,"--> Bu mantiq !")
+
+
+
